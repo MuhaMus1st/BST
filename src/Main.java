20e3d9a56bf70a96903f33f16fe764e455204f59
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-05-13 08:09:42 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        HashTable<MyTestingClass, Student> table = new HashTable<>();
+        for (int i = 0; i < 10000; i++) {
+            MyTestingClass key = new MyTestingClass("Key" + i);
+            Student value = new Student("Name" + i);
+            table.put(key, value);
+        }
 
+        table.printBucketSizes();
+
+        BST<Integer, String> tree = new BST<>();
+        tree.put(5, "Five");
+        tree.put(2, "Two");
+        tree.put(8, "Eight");
+        tree.put(1, "One");
+        tree.put(3, "Three");
+
+        System.out.println("Size: " + tree.size());
+
+        System.out.println("In order traversal:");
+        for (Integer key : tree.inOrderTraversal()) {
+            System.out.println("Key is " + key + " and value is " + tree.get(key));
+        }
     }
 }
